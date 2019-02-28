1ca1781c5352f5167a9f6bcacf90b816cfda1bfd
dziper <dziper@me.com>
2019-02-27 22:38:00 -0800

    Create Utils

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
     //Main for Data Parser
 
     public static void main(String[] args) {
-
+        String data = Utils.readFileAsString("data/2016_Presidential_Results.csv");
+        System.out.println(data);
     }
 }
