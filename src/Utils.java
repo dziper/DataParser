3b015fd183e0842a6dc82abf5d9097106ec654c6
dziper <dziper@me.com>
2019-03-02 22:05:03 -0800

    Implemented cleanLine() for Utils

--- src/Utils.java
+++ src/Utils.java
@@ -21,6 +21,37 @@ public class Utils {
     }
 
     public static ArrayList<ElectionResult> parse2016ElectionResults(String results){
+        ArrayList<ElectionResult> out = new ArrayList<>();
+        String[] lines = results.split(System.getProperty("line.separator"));
 
+        for (int i = 0; i < lines.length; i++) {
+            ElectionResult res = new ElectionResult();
+            parseLine(res, lines[i]);
+            out.add(res);
+        }
+        return out;
+    }
+
+    private static void parseLine(ElectionResult res, String line) {
+        String cleanData = cleanLine(line);
+    }
+
+    private static String cleanLine(String line) {
+        boolean openQuote = false;
+        for (int i = 0; i < line.length(); i++) {
+            if(openQuote) {
+                if (line.substring(i, i + 1).equals("\"")) {
+                    openQuote !=;
+                }
+
+                if(line.substring(i,i+1).equals(",") || line.substring(i, i + 1).equals("\"") || line.substring(i, i + 1).equals("%")){
+                    line = line.substring(0, i) + line.substring(i + 1);
+                    i--;
+                }
+            }
+
+        }
+
+        return line;
     }
 }
